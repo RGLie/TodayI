01fffe431facc6df9c5eb45e481b94d7be27980e
Jaehyun Jeong <j2hyeon02@kaist.ac.kr>
2022-10-31 13:36:47 +0000

    color setting, test

---
+++ todayi/lib/pages/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: 100,
+        height: 100,
+        color: Colors.red,
+        child: Text('asdf'),
+      ),
+    );
+  }
+}
---
+++ todayi/lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorLibrary{
+  static Color mainThemeColor = Color(0xffFFEDD2);
+  static Color textThemeColor = Color(0xff3A2000);
+
+}
