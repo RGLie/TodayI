121cbff73c824c71f665940ad982080d434699e8
Jaehyun Jeong <j2hyeon02@kaist.ac.kr>
2022-11-14 23:24:16 +0000

    should solve accountbar release err

--- todayi/lib/pages/main_page_sidebar.dart
+++ todayi/lib/pages/main_page_sidebar.dart
@@ -21,15 +21,26 @@ class _MainPageSideBarState extends State<MainPageSideBar> {
     var sidebar_provider = Provider.of<SideBarProvider>(context);
 
     final List<String> sidebar_list = ['노트', '타임라인', '태그', '보내기', '소셜'];
-    final List<IconData> icon_list = [Icons.event_note, Icons.timeline, Icons.tag, Icons.upgrade, Icons.public];
+    final List<IconData> icon_list = [
+      Icons.event_note,
+      Icons.timeline,
+      Icons.tag,
+      Icons.upgrade,
+      Icons.public
+    ];
     //user_data.name
     return Scaffold(
       body: Row(
         children: [
           Container(
+            constraints: BoxConstraints(
+              minWidth: 250,
+            ),
             width: MediaQuery.of(context).size.width * 0.15,
+            height: double.infinity,
             color: ColorLibrary.textThemeColor,
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Container(
                   padding: EdgeInsets.all(20),
@@ -40,66 +51,78 @@ class _MainPageSideBarState extends State<MainPageSideBar> {
                         MaterialPageRoute(builder: (context) => HomePage()),
                       );
                     },
-                    child: Text('Today I',
-                      style: TextStyle(color: Colors.white, fontSize: 45, fontWeight: FontWeight.w700),
+                    child: Text(
+                      'Today I',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 45,
+                          fontWeight: FontWeight.w700),
                     ),
                   ),
                 ),
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        padding: EdgeInsets.only(left:30),
-                        child: Text('Contents', 
-                          style: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.w300),
-                        )
-                      ),
-                      SizedBox(
-                        height: 400,
-                        child: ListView.builder(
-                          itemCount: sidebar_list.length,
-                          padding: EdgeInsets.only(left: 20, right:20, top:8, bottom: 8),
-                          itemBuilder: (BuildContext context, int index) {
-                            return Material(
-                              color: ColorLibrary.textThemeColor,
-                              child: InkWell(
-                                // focusColor: Colors.white,
-                                //splashColor: Colors.white,
-                                // hoverColor: Colors.white,
-                                // highlightColor: Colors.white,
-                                onTap: () {
-                                  sidebar_provider.changeIndex(index);
-                                },
-                                child: Container(
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                        padding: EdgeInsets.only(left: 30),
+                        child: Text(
+                          'Contents',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.w300),
+                        )),
+                    SizedBox(
+                      height: 300,
+                      child: ListView.builder(
+                        itemCount: sidebar_list.length,
+                        padding: EdgeInsets.only(
+                            left: 20, right: 20, top: 8, bottom: 8),
+                        itemBuilder: (BuildContext context, int index) {
+                          return Material(
+                            color: ColorLibrary.textThemeColor,
+                            child: InkWell(
+                              // focusColor: Colors.white,
+                              //splashColor: Colors.white,
+                              // hoverColor: Colors.white,
+                              // highlightColor: Colors.white,
+                              onTap: () {
+                                sidebar_provider.changeIndex(index);
+                              },
+                              child: Container(
                                   height: 40,
                                   width: double.infinity,
                                   child: Row(
-                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
                                     mainAxisAlignment: MainAxisAlignment.start,
                                     children: [
                                       SizedBox(
                                         width: 10,
                                       ),
-                                      Icon(icon_list[index], color: Colors.white,),
+                                      Icon(
+                                        icon_list[index],
+                                        color: Colors.white,
+                                      ),
                                       SizedBox(
                                         width: 15,
                                       ),
-                                      Text(sidebar_list[index],
-                                        style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500),
+                                      Text(
+                                        sidebar_list[index],
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.w500),
                                       ),
                                     ],
-                                  )
-                                ),
-                              ),
-                            );
-                          },
-                        ),
+                                  )),
+                            ),
+                          );
+                        },
                       ),
-                      
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
                 Container(
                   width: double.infinity,
--- todayi/lib/widgets/main_home_page/account_bar.dart
+++ todayi/lib/widgets/main_home_page/account_bar.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todayi/data/user.dart';
+import 'package:todayi/pages/home_page.dart';
 
 class AccountBar extends StatefulWidget {
   const AccountBar({super.key});
@@ -10,43 +12,58 @@ class AccountBar extends StatefulWidget {
 }
 
 class _AccountBarState extends State<AccountBar> {
-  
   @override
   Widget build(BuildContext context) {
-    var user_data = Provider.of<TUser>(context);
+    //var user_data = Provider.of<TUser>(context);
     return Expanded(
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          SizedBox(
-            width: 30,
-          ),
+          // SizedBox(
+          //   width: 30,
+          // ),
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(user_data.name,
+              Text(
+                'user_data.name.toString()',
                 style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500
-                ),
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500),
               ),
               SizedBox(
                 height: 3,
               ),
-              Text(user_data.email,
+              Text(
+                'user_data.email.toString()',
                 style: TextStyle(
-                  color: Colors.white70,
-                  fontSize: 14,
-                  fontWeight: FontWeight.w400
-                ),
+                    color: Colors.white70,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400),
               )
             ],
-          )
+          ),
+          IconButton(
+              onPressed: () {
+                _signOut();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                );
+              },
+              icon: Icon(
+                Icons.logout,
+                color: Colors.white,
+              ))
         ],
       ),
     );
+  }
+
+  Future<void> _signOut() async {
+    await FirebaseAuth.instance.signOut();
   }
 }
