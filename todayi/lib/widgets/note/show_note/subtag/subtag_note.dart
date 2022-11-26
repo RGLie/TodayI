447772186480dc9b8083a409567a543436694825
Jaehyun Jeong <j2hyeon02@kaist.ac.kr>
2022-11-26 14:49:56 +0000

    subtag

---
+++ todayi/lib/widgets/note/show_note/subtag/subtag_note.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:todayi/utils/colors.dart';
+
+class SubtagNote extends StatelessWidget {
+  List<Widget> child;
+  String tagtext;
+
+  SubtagNote({
+    super.key, 
+    required this.child,
+    required this.tagtext,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding:
+        EdgeInsets.only(left: 15, right: 15, bottom: 15, top: 15),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: ColorLibrary.cardContentColor,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 10.0),
+            child: Text(
+              '# 에러',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 19),
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Container(
+            padding: EdgeInsets.all(15),
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: ColorLibrary.cardSubTagColor,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Text('Builder 오류 해결',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 16)),
+          )
+        ],
+      ));
+  }
+}
