0ec7f5f11217d8644aecdd81e08e00fa72e72158
Jaehyun Jeong <j2hyeon02@kaist.ac.kr>
2022-12-14 12:04:56 +0000

    addTag number and overflow

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 ## Run
 flutter run --release -d web-server --web-hostname=0.0.0.0 --web-port=3000
+
+firebase deploy --only hosting
--- todayi/lib/widgets/note/addtag.dart
+++ todayi/lib/widgets/note/addtag.dart
@@ -1,4 +1,5 @@
 import 'dart:html';
+import 'dart:math';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -19,17 +20,11 @@ class _AddTagState extends State<AddTag> {
   Widget build(BuildContext context) {
     var user_data = Provider.of<TUser>(context);
     List<Widget> addTagWidgetList = [];
-    for (var tag in user_data.taglist) {
-      addTagWidgetList.add(
-        SizedBox(
-          width:10
-        )
-      );
+    for (var i = 0; i < min(user_data.taglist.length, 5); i++) {
+      addTagWidgetList.add(SizedBox(width: 10));
       addTagWidgetList.add(
         InkWell(
-          onTap:(){
-            
-          },
+          onTap: () {},
           child: Container(
             height: 35,
             padding: EdgeInsets.only(left: 12, right: 12),
@@ -45,26 +40,24 @@ class _AddTagState extends State<AddTag> {
               ],
             ),
             child: Center(
-              child: Text(
-                tag,
-                style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    color: Colors.black,
-                    fontSize: 15),
+                child: Text(
+              user_data.taglist[i].length>8?user_data.taglist[i].substring(0,8)+'...':user_data.taglist[i],
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                  fontSize: 15),
             )),
           ),
         ),
       );
-      
     }
 
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         InkWell(
-          onTap: () {
-            
-          },
+          onTap: () {},
           child: Container(
             height: 35,
             padding: EdgeInsets.only(left: 12, right: 12),
@@ -83,13 +76,13 @@ class _AddTagState extends State<AddTag> {
                 child: Text(
               '태그 생성',
               style: TextStyle(
-                  fontWeight: FontWeight.w600, color: Colors.black, fontSize: 15),
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                  fontSize: 15),
             )),
           ),
         ),
-        Row(
-          children: addTagWidgetList
-        )
+        Row(children: addTagWidgetList)
       ],
     );
   }
