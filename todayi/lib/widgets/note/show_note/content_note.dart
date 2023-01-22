25b2e97a8df03f4c36f575725077cc9e27248c5b
Jaehyun Jeong <j2hyeon02@kaist.ac.kr>
2023-01-22 08:07:20 +0000

    add dismissible

--- todayi/lib/widgets/note/show_note/content_note.dart
+++ todayi/lib/widgets/note/show_note/content_note.dart
@@ -30,110 +30,124 @@ class ContentNote extends StatelessWidget {
   Widget build(BuildContext context) {
     var user_data = Provider.of<TUser>(context);
     var today_note = Provider.of<NoteProvider>(context);
-    return Column(
-      children: [
-        SizedBox(
-          height: 15,
-        ),
-        Container(
-          padding: EdgeInsets.only(left: 15, right: 15, top: 15),
-          width: double.infinity,
-          decoration: BoxDecoration(
-            color: cardcolor,
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              MarkdownBody(
-                  onTapLink: (text, href, title) {
-                    href != null ? launchUrl(Uri.parse(href)) : null;
-                  },
-                  selectable: true,
-                  data: content,
-                  styleSheet: MarkdownStyleSheet(
-                    p: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.w500,
-                        fontSize: 17),
-                    h4: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.w700,
-                        fontSize: 20),
-                    h3: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.w700,
-                        fontSize: 22),
-                    h2: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.w700,
-                        fontSize: 25),
-                    h1: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.w700,
-                        fontSize: 28),
-                    // code: TextStyle(
-                    //   backgroundColor: Colors.black,
-                    //   color: Colors.white,
-                    //   fontSize: 16
-
-                    // )
-                  ),
-                  key: const Key("defaultmarkdownformatter"),
-                  builders: {
-                    'code': CodeElementBuilder(),
-                  }),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  IconButton(
-                      onPressed: () {
-                        Clipboard.setData(ClipboardData(text:content));
-                        Fluttertoast.showToast(msg: '복사되었습니다.',
-                          gravity: ToastGravity.BOTTOM,
-                          backgroundColor: Colors.white,
-                          webBgColor: "linear-gradient(to right, #3A2000, #3A2000)",
-                          webPosition: 'center',
+    String contentID = subtag==''?
+      tagname + '?' + today_note.today_date + '?' + count.toString()
+      :tagname + '.'+ subtag+'?' + today_note.today_date + '?' + count.toString();
+    
+    return Dismissible(
+      key: Key(contentID),
+      background: Container(color: Colors.redAccent),
+      onDismissed: (direction) {
+        CollectionReference contents = FirebaseFirestore.instance
+                            .collection('users')
+                            .doc(user_data.uid)
+                            .collection('contents');
                           
-                          fontSize: 20.0,
-                          textColor: Colors.white,
-                          toastLength: Toast.LENGTH_SHORT
-                        );
-                      },
-                      icon: Icon(
-                        Icons.content_copy_outlined,
-                        color: ColorLibrary.textThemeColor,
-                        size: 18,
-                      )),
-                  IconButton(
-                      onPressed: () {
-                        CollectionReference contents = FirebaseFirestore.instance
-                          .collection('users')
-                          .doc(user_data.uid)
-                          .collection('contents');
-                        String contentID = subtag==''?
-                          tagname + '?' + today_note.today_date + '?' + count.toString()
-                          :tagname + '.'+ subtag+'?' + today_note.today_date + '?' + count.toString();
-                        contents.doc(contentID).delete();
-                        
-                      },
-                      icon: Icon(
-                        Icons.delete_outline,
-                        color: ColorLibrary.textThemeColor,
-                        size: 22,
-                      )),
-                ],
-              ),
-            ],
+                          contents.doc(contentID).delete();
+      },
+      child: Column(
+        children: [
+          SizedBox(
+            height: 15,
           ),
-
-          // child: Text(content,
-          //     style: TextStyle(
-          //         color: Colors.black,
-          //         fontWeight: FontWeight.w500,
-          //         fontSize: 17)),
-        ),
-      ],
+          Container(
+            padding: EdgeInsets.only(left: 15, right: 15, top: 15),
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: cardcolor,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                MarkdownBody(
+                    onTapLink: (text, href, title) {
+                      href != null ? launchUrl(Uri.parse(href)) : null;
+                    },
+                    selectable: true,
+                    data: content,
+                    styleSheet: MarkdownStyleSheet(
+                      p: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 17),
+                      h4: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 20),
+                      h3: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 22),
+                      h2: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 25),
+                      h1: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 28),
+                      // code: TextStyle(
+                      //   backgroundColor: Colors.black,
+                      //   color: Colors.white,
+                      //   fontSize: 16
+    
+                      // )
+                    ),
+                    key: const Key("defaultmarkdownformatter"),
+                    builders: {
+                      'code': CodeElementBuilder(),
+                    }),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    IconButton(
+                        onPressed: () {
+                          Clipboard.setData(ClipboardData(text:content));
+                          Fluttertoast.showToast(msg: '복사되었습니다.',
+                            gravity: ToastGravity.BOTTOM,
+                            backgroundColor: Colors.white,
+                            webBgColor: "linear-gradient(to right, #3A2000, #3A2000)",
+                            webPosition: 'center',
+                            
+                            fontSize: 20.0,
+                            textColor: Colors.white,
+                            toastLength: Toast.LENGTH_SHORT
+                          );
+                        },
+                        icon: Icon(
+                          Icons.content_copy_outlined,
+                          color: ColorLibrary.textThemeColor,
+                          size: 18,
+                        )),
+                    IconButton(
+                        onPressed: () {
+                          CollectionReference contents = FirebaseFirestore.instance
+                            .collection('users')
+                            .doc(user_data.uid)
+                            .collection('contents');
+                          
+                          contents.doc(contentID).delete();
+                          
+                        },
+                        icon: Icon(
+                          Icons.delete_outline,
+                          color: ColorLibrary.textThemeColor,
+                          size: 22,
+                        )),
+                  ],
+                ),
+              ],
+            ),
+    
+            // child: Text(content,
+            //     style: TextStyle(
+            //         color: Colors.black,
+            //         fontWeight: FontWeight.w500,
+            //         fontSize: 17)),
+          ),
+        ],
+      ),
     );
   }
 }
