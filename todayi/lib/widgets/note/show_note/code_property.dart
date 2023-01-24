67203af7c96a9915df26779972c2d9ee46003883
Jaehyun Jeong <j2hyeon02@kaist.ac.kr>
2023-01-24 10:25:25 +0000

    design all changed

--- todayi/lib/widgets/note/show_note/code_property.dart
+++ todayi/lib/widgets/note/show_note/code_property.dart
@@ -1,5 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:provider/provider.dart';
+import 'package:todayi/data/user.dart';
+import 'package:todayi/providers/note/note_provider.dart';
 import 'package:todayi/utils/code_element.dart';
 import 'package:todayi/utils/colors.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -37,6 +42,12 @@ class CodePropertyNote extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String codeblock = '```$language\n$code';
+    var user_data = Provider.of<TUser>(context);
+    var today_note = Provider.of<NoteProvider>(context);
+    String contentID = subtag==''?
+      tagname + '?' + today_note.today_date + '?' + count.toString()
+      :tagname + '.'+ subtag+'?' + today_note.today_date + '?' + count.toString();
+    
     String txt = '';
 
     if (property1) {
@@ -60,99 +71,64 @@ class CodePropertyNote extends StatelessWidget {
         SizedBox(
           height: 15,
         ),
-        Container(
-          padding: EdgeInsets.only(top: 15),
-          width: double.infinity,
-          decoration: BoxDecoration(
-            color: cardcolor,
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(left: 15, right: 15),
-                child: MarkdownBody(
-                    onTapLink: (text, href, title) {
-                      href != null ? launchUrl(Uri.parse(href)) : null;
-                    },
-                    selectable: true,
-                    data: content,
-                    styleSheet: MarkdownStyleSheet(
-                      p: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.w500,
-                          fontSize: 17),
-                      h4: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.w700,
-                          fontSize: 20),
-                      h3: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.w700,
-                          fontSize: 22),
-                      h2: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.w700,
-                          fontSize: 25),
-                      h1: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.w700,
-                          fontSize: 28),
-                      // code: TextStyle(
-                      //   backgroundColor: Colors.black,
-                      //   color: Colors.white,
-                      //   fontSize: 16
-
-                      // )
-                    ),
-                    key: const Key("defaultmarkdownformatter"),
-                    builders: {
-                      'code': CodeElementBuilder(),
-                    }),
+        Dismissible(
+          key: Key(contentID),
+            background: Container(
+              
+              decoration: BoxDecoration(
+                color: Colors.redAccent,
+                borderRadius: BorderRadius.circular(10),
               ),
-              Padding(
-                padding: EdgeInsets.only(left: 7, right: 7, top: 13,),
-                child: Container(
-                  padding:
-                      EdgeInsets.only(left: 20, right: 20, top: 6, ),
-                  decoration: BoxDecoration(
-                    color: Color(0xff282c34),
-                    borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10)),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        language,
-                        style: TextStyle(color: Colors.white, fontSize: 10),
-                      ),
-                      InkWell(
-                        onTap: () {},
-                        child: Icon(
-                          Icons.content_copy_outlined,
-                          color: Colors.white,
-                          size: 10,
-                        ),
-                      )
-                    ],
-                  ),
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.all(15),
+                  child: Icon(
+                    Icons.delete,
+                    color: Colors.white,
+
+                  )
                 ),
               ),
-              Padding(
-                padding: EdgeInsets.only(left: 7, right: 7),
-                child: Container(
+            ),
+            onDismissed: (direction) {
+
+              CollectionReference contents = FirebaseFirestore.instance
+                .collection('users')
+                .doc(user_data.uid)
+                .collection('contents');
+              
+              contents.doc(contentID).delete();
+
+              Fluttertoast.showToast(msg: '삭제 되었습니다.',
+                gravity: ToastGravity.BOTTOM,
+                backgroundColor: Colors.white,
+                webBgColor: "linear-gradient(to right, #3A2000, #3A2000)",
+                webPosition: 'center',
+                
+                fontSize: 20.0,
+                textColor: Colors.white,
+                toastLength: Toast.LENGTH_SHORT
+              );
+            },
+          direction: DismissDirection.endToStart,
+          child: Container(
+            padding: EdgeInsets.only(top: 15, bottom: 10),
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: cardcolor,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
                   padding: EdgeInsets.only(left: 15, right: 15),
-                  decoration: BoxDecoration(
-                      color: Color(0xff282c34),
-                      borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10), bottomRight: Radius.circular(10)),
-                    ),
                   child: MarkdownBody(
                       onTapLink: (text, href, title) {
                         href != null ? launchUrl(Uri.parse(href)) : null;
                       },
                       selectable: true,
-                      data: codeblock,
+                      data: content,
                       styleSheet: MarkdownStyleSheet(
                         p: TextStyle(
                             color: Colors.black,
@@ -178,7 +154,7 @@ class CodePropertyNote extends StatelessWidget {
                         //   backgroundColor: Colors.black,
                         //   color: Colors.white,
                         //   fontSize: 16
-                
+        
                         // )
                       ),
                       key: const Key("defaultmarkdownformatter"),
@@ -186,49 +162,85 @@ class CodePropertyNote extends StatelessWidget {
                         'code': CodeElementBuilder(),
                       }),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 15, right: 15),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      txt,
-                      style: TextStyle(
-                          color: ColorLibrary.textThemeColor,
-                          fontWeight: FontWeight.w500,
-                          fontSize: 13),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.content_copy_outlined,
-                              color: ColorLibrary.textThemeColor,
-                              size: 18,
-                            )),
-                        IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.delete_outline,
-                              color: ColorLibrary.textThemeColor,
-                              size: 22,
-                            )),
-                      ],
-                    ),
-                  ],
+                SizedBox(
+                    height: 15,
+                  ),
+                Padding(
+                  padding: EdgeInsets.only(left: 10, right: 10),
+                  child: Container(
+                    padding: EdgeInsets.only(left: 15, right: 15),
+                    decoration: BoxDecoration(
+                        color: Color(0xff282c34),
+                        borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10), bottomLeft: Radius.circular(10), bottomRight: Radius.circular(10)),
+                        ),
+                    child: MarkdownBody(
+                        onTapLink: (text, href, title) {
+                          href != null ? launchUrl(Uri.parse(href)) : null;
+                        },
+                        selectable: true,
+                        data: codeblock,
+                        styleSheet: MarkdownStyleSheet(
+                          p: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 17),
+                          h4: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 20),
+                          h3: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 22),
+                          h2: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 25),
+                          h1: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 28),
+                          // code: TextStyle(
+                          //   backgroundColor: Colors.black,
+                          //   color: Colors.white,
+                          //   fontSize: 16
+                  
+                          // )
+                        ),
+                        key: const Key("defaultmarkdownformatter"),
+                        builders: {
+                          'code': CodeElementBuilder(),
+                        }),
+                  ),
                 ),
-              ),
-            ],
+                SizedBox(
+                    height: 10,
+                  ),
+                Padding(
+                  padding: EdgeInsets.only(left: 15, right: 15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        txt,
+                        style: TextStyle(
+                            color: ColorLibrary.textThemeColor,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 13),
+                      ),
+                      
+                    ],
+                  ),
+                ),
+              ],
+            ),
+        
+            // child: Text(content,
+            //     style: TextStyle(
+            //         color: Colors.black,
+            //         fontWeight: FontWeight.w500,
+            //         fontSize: 17)),
           ),
-
-          // child: Text(content,
-          //     style: TextStyle(
-          //         color: Colors.black,
-          //         fontWeight: FontWeight.w500,
-          //         fontSize: 17)),
         ),
       ],
     );
